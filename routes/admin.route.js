3c72b4976f4cf57c65d488dea547f83a7230ca73
Rajendra3049 <Rajendrapatel3049@gmail.com>
2023-05-01 10:11:49 +0530

    admin dashboard

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { adminModel } = require("../models/admin.model");
 const { orderModel } = require("../models/order.model");
+const { productModel } = require("../models/product.model");
 const jwt = require("jsonwebtoken");
 
 const adminRoutes = express.Router();
@@ -11,6 +12,58 @@ adminRoutes.get("/", async (req, res) => {
     res.status(200).send({ admin: adminFound });
   } catch (error) {
     res.status(500).send({ msg: "Error in getting admin" });
+  }
+});
+
+adminRoutes.get("/dashboard", async (req, res) => {
+  try {
+    let productsLength = 0;
+    let ordersLength = 0;
+    let category = [];
+
+    await productModel
+      .countDocuments({})
+      .then((productCount) => {
+        productsLength = productCount;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    await productModel
+      .aggregate([
+        {
+          $group: {
+            _id: "$category",
+            count: { $sum: 1 },
+          },
+        },
+      ])
+      .then((results) => {
+        category = results;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    await orderModel
+      .countDocuments({})
+      .then((orderCount) => {
+        ordersLength = orderCount;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    res
+      .status(200)
+      .send({
+        products: productsLength,
+        Orders: ordersLength,
+        category: category,
+      });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ msg: "Error in getting admin", error: error });
   }
 });
 
